8b65985d3e9db90165d6592ccdb030e3718e340a
facet-it <facet.it.be@gmail.com>
2017-12-28 11:08:04 +0100

    new project structure

---
+++ AkkaFileReader/src/main/java/be/app/akkafilereader/input/cinema/CinemaActor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.app.akkafilereader.input.cinema;
+
+import akka.actor.AbstractActor;
+
+public class CinemaActor extends AbstractActor{
+
+    @Override
+    public Receive createReceive() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
